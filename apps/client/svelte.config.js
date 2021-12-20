38e06f7ea054ad61d319329a36352fb47483363c
Alyx Mote <figments@outlook.com>
2021-12-20 14:33:39 -0800

    add vercel

--- apps/client/svelte.config.js
+++ apps/client/svelte.config.js
@@ -1,36 +1,18 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: [
-        preprocess({
-            postcss: true,
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-            scss: {
-                prependData: '@import "src/variables.scss";',
-            },
-        }),
-    ],
+	kit: {
+		adapter: adapter(),
 
-    kit: {
-        adapter: vercel(),
-
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte',
-
-        vite: {
-            css: {
-                preprocessorOptions: {
-                    scss: {
-                        additionalData: '@import "src/variables.scss";',
-                    },
-                },
-            },
-        },
-    },
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
 
 export default config;
