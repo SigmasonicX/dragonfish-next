5cd0b05e8b032234b0d0b1f2910ac42ea01ad39e
Alyx Mote <figments@outlook.com>
2021-12-20 14:36:45 -0800

    packages being added

--- apps/client/svelte.config.js
+++ apps/client/svelte.config.js
@@ -3,16 +3,34 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: preprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true,
 
-    kit: {
-        adapter: vercel(),
+			scss: {
+				prependData: '@import "src/variables.scss";'
+			}
+		})
+	],
 
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte',
-    },
+	kit: {
+		adapter: vercel(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@import "src/variables.scss";'
+					}
+				}
+			}
+		}
+	}
 };
 
 export default config;
