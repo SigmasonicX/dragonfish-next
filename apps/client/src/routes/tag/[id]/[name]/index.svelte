23931ff84d62ec785f817cb857f85986259b00fb
SigmasonicX <sigmasonicx@gmail.com>
2022-03-23 23:44:01 -0700

    Added header but page doesn't update when click new tag - Header needs better format

--- apps/client/src/routes/tag/[id]/[name]/index.svelte
+++ apps/client/src/routes/tag/[id]/[name]/index.svelte
@@ -4,45 +4,79 @@
     import Paginator from "$lib/components/ui/misc/Paginator.svelte";
 
     import type { Content } from "$lib/models/content";
-    import type { TagsTree } from "$lib/models/content/works";
+    import type { TagsModel, TagsTree } from "$lib/models/content/works";
 
     import type { PaginateResult } from "$lib/models/util";
     import { app } from "$lib/repo/app.repo";
-    import { search, setFilter } from "$lib/repo/search.repo";
+    import { search, setCurrentTag, setFilter } from "$lib/repo/search.repo";
     import { findRelatedContent } from "$lib/services/search.service";
     import { fetchDescendants } from "$lib/services/tags.service";
     import { updateUrlParams } from "$lib/util";
     import type { Load } from "@sveltejs/kit";
     import { get } from "svelte/store";
+    import { slugify } from '$lib/util';
+    import { onMount } from "svelte";
 
-    let contentResults: PaginateResult<Content> = null;
-    let tagsTree: TagsTree = null;
-    setFilter(get(app).filter);
+    let tagId: string = null;
+    let page: number = 1;
 
     export const load: Load = async ({ params, url }) => {
-        get(search).tagIds = [params.id];
-        get(search).page = url.searchParams.has('page') ? +url.searchParams.get('page') : 1;
-
-        contentResults = await findRelatedContent(get(search));
-        fetchDescendants(params.id).subscribe((tree) => {
-            tagsTree = tree;
-        });
+        console.log("Calling load for tag page, id=" + params.id);
+        tagId = params.id;
+        page =  url.searchParams.has('page') ? +url.searchParams.get('page') : 1;
+        setCurrentTag(tagId, page);
         return {
             props: {
-                content: contentResults
+                tagId: tagId,
             },
         };
     }
+</script>
+<script lang="ts">
+    let contentResults: PaginateResult<Content> = null;
+    let tagsTree: TagsTree = null;
+    let parent: TagsModel = null;
+
+    $: setFilter($app.filter);
+
+    onMount(async () => {
+        contentResults = await findRelatedContent($search);
+        tagsTree = await fetchDescendants($search.tagIds[0]);
+        parent = tagsTree.parent as TagsModel;
+    })
 
     async function setNewPage(currPage: number) {
-        get(search).page = currPage;
+        $search.page = currPage;
         updateUrlParams({
             page: currPage > 1 ? currPage.toString() : null,
         })
-        contentResults = await findRelatedContent(get(search));
+        contentResults = await findRelatedContent($search);
     }
 </script>
 
+{#if tagsTree}
+    <div class="text-2xl">
+        {#if parent}
+            <a
+                href="/tag/{parent._id}/{slugify(parent.name)}"
+            >{parent.name}</a>
+            &nbsp;— 
+        {/if}
+        <span>{tagsTree.name}</span>
+    </div>
+    {#if tagsTree.children && tagsTree.children.length > 0}
+        <span>Child Tags: </span>
+        {#each tagsTree.children as child, index}
+            <a
+                href="/tag/{child._id}/{slugify(child.name)}"
+            >{child.name}</a>
+            {#if index < tagsTree.children.length - 1}
+                <span>, </span>
+            {/if}
+        {/each}
+    {/if}
+    <p>{tagsTree.desc}</p>
+{/if}
 <div class="w-full h-[calc(100vh-51px)] md:h-screen overflow-y-auto">
     {#if contentResults && contentResults.totalDocs > 0}
         <div
